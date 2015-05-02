7466eedfbea92bd49b37154c0452d2fa7646dad5
Rene Polo <renejpolo@me.com>
2015-05-01 22:26:29 -0400

    server js moved to root

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 
-app.use(express.static(__dirname + '../../app'));
+app.use(express.static(__dirname + '/app'));
 
 
 app.get('/blog', function (req, res) {
